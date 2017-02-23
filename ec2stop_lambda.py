675b1379965086178490bd0a501edb2cedde4438
SKazuki <kazuki.suetake@gmail.com>
2017-02-23 18:47:32 +0900

    you don't have to mind

--- ec2stop_lambda.py
+++ ec2stop_lambda.py
@@ -1,12 +1,11 @@
 import boto3
 
 # Enter the region your instances are in, e.g. 'us-east-1'
-region = 'XX-XXXXX-X'
+region = 'XXXXXXX'
 
 # Enter your instances here: ex. ['X-XXXXXXXX', 'X-XXXXXXXX']
-instances = ['X-XXXXXXXX']
-
+instances = ['X-XXXXXXX']
 def lambda_handler(event, context):
-    ec2 = boto3.client('ec2')
+    ec2 = boto3.client('ec2', region_name=region)
     ec2.stop_instances(InstanceIds=instances)
     print 'stopped your instances: ' + str(instances)
