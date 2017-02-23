fb3161a5173f2308615e49ad11f48656dd611913
SKazuki <kazuki.suetake@gmail.com>
2017-02-23 20:53:21 +0900

    add Error handling and Try sentence

--- ec2start_lambda.py
+++ ec2start_lambda.py
@@ -1,11 +1,22 @@
 import boto3
+import time
+from botocore.client import ClientError
 
 # Enter the region your instances are in, e.g. 'us-east-1'
-region = 'XX-XXXXX-X'
+region = 'XXXXXXX'
 
 # Enter your instances here: ex. ['X-XXXXXXXX', 'X-XXXXXXXX']
-instances = ['X-XXXXXXXX']
+instances = ['X-XXXXXXX']
+
 def lambda_handler(event, context):
     ec2 = boto3.client('ec2', region_name=region)
-    ec2.start_instances(InstanceIds=instances)
-    print 'started your instances: ' + str(instances)
+    # Try 2 times
+    for i in range(1, 3):
+        try:
+            ec2.start_instances(InstanceIds=instances)
+            print 'started your instances: ' + str(instances)
+            return True
+        except ClientError as e:
+            print str(e)
+        time.sleep(1)
+    raise Exception('cannot start instance ' + str(instances))
--- ec2stop_lambda.py
+++ ec2stop_lambda.py
@@ -1,11 +1,22 @@
 import boto3
+import time
+from botocore.client import ClientError
 
 # Enter the region your instances are in, e.g. 'us-east-1'
 region = 'XXXXXXX'
 
 # Enter your instances here: ex. ['X-XXXXXXXX', 'X-XXXXXXXX']
 instances = ['X-XXXXXXX']
+
 def lambda_handler(event, context):
     ec2 = boto3.client('ec2', region_name=region)
-    ec2.stop_instances(InstanceIds=instances)
-    print 'stopped your instances: ' + str(instances)
+    # Try 2 times
+    for i in range(1, 3):
+        try:
+            ec2.stop_instances(InstanceIds=instances)
+            print 'stopped your instances: ' + str(instances)
+            return True
+        except ClientError as e:
+            print str(e)
+        time.sleep(1)
+    raise Exception('cannot stop instance ' + str(instances))
